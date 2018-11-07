6977ecd897c9efa242d830cb76e6f49fce3b4884
Cristian Bodnar <crisbodnar96@gmail.com>
2018-11-07 12:31:14 +0000

    Finish the map reduce task

--- map_reduce/map_reduce.py
+++ map_reduce/map_reduce.py
@@ -1,24 +1,27 @@
 import numpy as np
 import tensorflow as tf
 
-# TODO create a cluster spec matching your server spec
-
+cluster_spec = tf.train.ClusterSpec({"local": ["localhost:2222", "localhost:2223"]})
 task_input = tf.placeholder(tf.float32, 100)
 
 # First part: compute mean of half of the input data
 with tf.device("/job:local/task:0"):
-    local_input = tf.slice(task_input, [50], [-1])
-    local_mean = tf.reduce_mean(local_input)
+    local_input_task0 = tf.slice(task_input, [50], [-1])
+    local_mean_task0 = tf.reduce_mean(local_input_task0)
 
 # TODO do another half of the computation using another device
+with tf.device("/job:local/task:1"):
+    local_input_task1 = tf.slice(task_input, [0], [50])
+    local_mean_task1 = tf.reduce_mean(local_input_task1)
+
 # TODO compute the overall result by combining both results
-
-
+global_mean = (local_mean_task0 + local_mean_task1) / 2.0
 
 # TODO Fill in the session specification
-with tf.Session() as sess:
+with tf.Session("grpc://localhost:2222") as sess:
     # Sample some data for the computation
     data = np.random.random(100)
 
-    # TODO run the session to compute the overall using your workers
     # and the input data. Output the result.
+    mean = sess.run(global_mean, feed_dict={task_input: data})
+    print(mean)
